b0a0433f067b6404556b44a78b5b1bb66597d581
shireknight <bvalentine1980@gmail.com>
2016-01-20 17:41:32 -0500

    Created daily trip
    Debugged app launch.

---
+++ dailytrip.py
@@ -0,0 +1,37 @@
+import os, sys
+from Tkinter import *
+
+class App:
+
+    def __init__(self, master):
+        self.frame = Frame(master)
+        self.frame.grid()
+        f2 = Frame(master, width=200, height=100)
+        f2.grid(row=0, column=1)
+        newTripBtn = Button(self.frame, text="New Trip", command=self.createNewTrip)
+        quitBtn = Button(self.frame, text="Quit", command=self.quit)
+        newTripBtn.pack(side=LEFT)
+        quitBtn.pack(side=RIGHT)
+
+    #def mkWidgets(self):
+
+
+
+
+    def createNewTrip(self):
+        Trip()
+
+    def quit(self):
+        sys.exit()
+
+
+class Trip:
+
+    def __init__(self):
+        print 'new trip instance'
+
+
+if __name__ == "__main__":
+    top = Tk()
+    app = App(top)
+    top.mainloop()
